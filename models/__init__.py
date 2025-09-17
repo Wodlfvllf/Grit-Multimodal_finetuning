b53ff8d1f06db1eed77a08b5925ed3d5775ef6ff
Wodlfvllf <shuklashashankshekhar863@gmail.com>
2025-09-17 06:14:22 +0000

    refactoring

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,10 @@
 
 from .grit_layer import LinearWithGRIT
-from .grit_model import GRITModel, GRITConfig
+from .grit_model import GRITModel
 from .replace_grit_modules import replace_linear_with_grit
 
 __all__ = [
     "LinearWithGRIT",
     "GRITModel",
-    "GRITConfig",
     "replace_linear_with_grit",
 ]
